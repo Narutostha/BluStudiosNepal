aff9a77a557141dbf1346f8c5737003417b7aeaa
Narutostha <bhuwanshrestha475@gmail.com>
2025-01-29 14:58:47 +0545

    [id]

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -2,13 +2,16 @@ import { blogdata } from "@/assets/data/dummydata"
 import Banner from "@/components/Banner"
 import { Title, TitleSm } from "@/components/common/Title"
 import Head from "next/head"
-import { useRouter } from "next/router"
 import React from "react"
 
-const SinglePost = () => {
-  const router = useRouter()
-  const { id } = router.query
-  const post = blogdata.find((post) => post.id === parseInt(id))
+const SinglePost = ({ post }) => {
+  if (!post) {
+    return (
+      <div className="container">
+        <div className="loading">Loading...</div>
+      </div>
+    )
+  }
 
   return (
     <>
@@ -22,31 +25,102 @@ const SinglePost = () => {
             <br />
             <Title title={post.title} className='title-bg' />
             <div className='img py'>
-              <img src={post.cover} alt={post.title} width='100%' height='100%' className='round' />
+              <img 
+                src={post.cover} 
+                alt={post.title} 
+                width='100%' 
+                height='100%' 
+                className='round' 
+              />
             </div>
             <div className='desc'>
               <TitleSm title='Phasellus at magna - elit tristique lacinia. Integer a justo vitae arcu fermentum consequat.' />
-              <p className='desc-p'> Nulla iaculis convallis fermentum. Suspendisse eget elit mauris. Phasellus velit nisi, lobortis quis nisi et, venenatis finibus velit. Integer non nibh eget arcu malesuada ullamcorper. Quisque congue ante in consequat auctor. Morbi ut accumsan eros. Mauris semper suscipit mattis. Cras pellentesque a urna ac dictum. Pellentesque blandit, sapien vel faucibus accumsan, ante dui imperdiet nisi, ut tincidunt nulla tortor nec purus.</p>
-              <p className='desc-p'>Suspendisse eget elit mauris. Phasellus velit nisi, lobortis quis nisi et, venenatis finibus velit. Integer non nibh eget arcu malesuada ullamcorper.</p>
-              <p className='desc-p'>Quisque congue ante in consequat auctor. Morbi ut accumsan eros. Mauris semper suscipit mattis. Cras pellentesque a urna ac dictum. Pellentesque blandit, sapien vel faucibus accumsan, ante dui imperdiet nisi, ut tincidunt nulla tortor nec purus.</p>
+              <p className='desc-p'> 
+                Nulla iaculis convallis fermentum. Suspendisse eget elit mauris. 
+                Phasellus velit nisi, lobortis quis nisi et, venenatis finibus velit. 
+                Integer non nibh eget arcu malesuada ullamcorper. Quisque congue ante 
+                in consequat auctor. Morbi ut accumsan eros. Mauris semper suscipit mattis. 
+                Cras pellentesque a urna ac dictum. Pellentesque blandit, sapien vel 
+                faucibus accumsan, ante dui imperdiet nisi, ut tincidunt nulla tortor nec purus.
+              </p>
+              <p className='desc-p'>
+                Suspendisse eget elit mauris. Phasellus velit nisi, lobortis quis nisi et, 
+                venenatis finibus velit. Integer non nibh eget arcu malesuada ullamcorper.
+              </p>
+              <p className='desc-p'>
+                Quisque congue ante in consequat auctor. Morbi ut accumsan eros. Mauris 
+                semper suscipit mattis. Cras pellentesque a urna ac dictum. Pellentesque 
+                blandit, sapien vel faucibus accumsan, ante dui imperdiet nisi, ut 
+                tincidunt nulla tortor nec purus.
+              </p>
             </div>
           </div>
+          
           <Banner />
 
           <div className='heading-title'>
             <div className='desc'>
               <TitleSm title='Integer a justo vitae arcu fermentum...' />
-
-              <p className='desc-p'> Phasellus nec tempor neque. In nec finibus lorem, in aliquet risus. Proin elit elit, cursus vel vulputate at, volutpat quis metus. Praesent at blandit tellus.</p>
-              <p className='desc-p'>Morbi finibus velit erat, a pulvinar lacus mollis sit amet. Nulla iaculis convallis fermentum. Suspendisse eget elit mauris. Phasellus velit nisi, lobortis quis nisi et, venenatis finibus velit. Integer non nibh eget arcu malesuada ullamcorper! Quisque congue ante in consequat auctor. Morbi ut accumsan eros. Mauris semper suscipit mattis. Cras pellentesque a urna ac dictum. Pellentesque blandit, sapien vel faucibus accumsan, ante dui imperdiet nisi, ut tincidunt nulla tortor nec purus.</p>
-              <p className='desc-p'>Suspendisse eget elit mauris. Phasellus velit nisi, lobortis quis nisi et, venenatis finibus velit. Integer non nibh eget arcu malesuada ullamcorper.</p>
-              <p className='desc-p'>Quisque congue ante in consequat auctor. Morbi ut accumsan eros. Mauris semper suscipit mattis. Cras pellentesque a urna ac dictum. Pellentesque blandit, sapien vel faucibus accumsan, ante dui imperdiet nisi, ut tincidunt nulla tortor nec purus.</p>
+              <p className='desc-p'> 
+                Phasellus nec tempor neque. In nec finibus lorem, in aliquet risus. 
+                Proin elit elit, cursus vel vulputate at, volutpat quis metus. 
+                Praesent at blandit tellus.
+              </p>
+              <p className='desc-p'>
+                Morbi finibus velit erat, a pulvinar lacus mollis sit amet. Nulla 
+                iaculis convallis fermentum. Suspendisse eget elit mauris. Phasellus 
+                velit nisi, lobortis quis nisi et, venenatis finibus velit. Integer 
+                non nibh eget arcu malesuada ullamcorper! Quisque congue ante in 
+                consequat auctor. Morbi ut accumsan eros. Mauris semper suscipit mattis. 
+                Cras pellentesque a urna ac dictum. Pellentesque blandit, sapien vel 
+                faucibus accumsan, ante dui imperdiet nisi, ut tincidunt nulla tortor nec purus.
+              </p>
+              <p className='desc-p'>
+                Suspendisse eget elit mauris. Phasellus velit nisi, lobortis quis nisi et, 
+                venenatis finibus velit. Integer non nibh eget arcu malesuada ullamcorper.
+              </p>
+              <p className='desc-p'>
+                Quisque congue ante in consequat auctor. Morbi ut accumsan eros. Mauris 
+                semper suscipit mattis. Cras pellentesque a urna ac dictum. Pellentesque 
+                blandit, sapien vel faucibus accumsan, ante dui imperdiet nisi, ut 
+                tincidunt nulla tortor nec purus.
+              </p>
             </div>
           </div>
         </div>
       </section>
     </>
   )
+}
+
+// Generate static paths for all blog posts
+export async function getStaticPaths() {
+  const paths = blogdata.map((post) => ({
+    params: { id: post.id.toString() }
+  }))
+
+  return {
+    paths,
+    fallback: false // or 'blocking' if you want to generate pages on demand
+  }
+}
+
+// Get the data for each blog post
+export async function getStaticProps({ params }) {
+  const post = blogdata.find((post) => post.id === parseInt(params.id))
+
+  // Return 404 if post not found
+  if (!post) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
 }
 
 export default SinglePost
